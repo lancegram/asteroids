1aa69962ddc2ee6e1f488a28893fcb81386c4886
lancegram <lance@examplemail.com>
2025-03-23 00:10:01 +0200

    initialisation, black screen only

---
+++ main.py
@@ -0,0 +1,27 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill(000000)
+
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
