e082b4aca7a0360f6862272849390ba2996fa0e7
lancegram <lance@examplemail.com>
2025-03-23 01:07:21 +0200

    added player sprite and circleshape class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 
 
@@ -14,16 +15,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
         screen.fill(000000)
-
+        player.draw(screen)
 
         pygame.display.flip()
-        dt = (clock.tick(60))/1000
+        dt = (clock.tick(60))/1000 #framerate limit: 60fps
 
 
 if __name__ == "__main__":
