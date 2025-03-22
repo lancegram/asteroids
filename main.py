9dd765a11b8909f41fecc66e2f88979f6484c61a
lancegram <lance@examplemail.com>
2025-03-23 00:36:13 +0200

    added framerate limit

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     print("Screen width: 1280")
     print("Screen height: 720")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True :
         for event in pygame.event.get():
@@ -21,6 +23,7 @@ def main():
 
 
         pygame.display.flip()
+        dt = (clock.tick(60))/1000
 
 
 if __name__ == "__main__":
