b162f4a18a6056d60bfbda97ae653f3abf14c77d
lancegram <lance@examplemail.com>
2025-03-23 14:09:54 +0200

    fixed soft presses ignore

--- main.py
+++ main.py
@@ -13,16 +13,25 @@ def main():
     print("Screen height: 720")
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
+    dt =0 
+    flag_updated_move = False
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    player.rotate(-dt)
+                if event.key == pygame.K_d:
+                    player.rotate(dt)
+                flag_updated_move = True
+            
+
         screen.fill(000000)
-        player.update(dt)
+        if not flag_updated_move == True: player.update(dt)
+        else: flag_updated_move = False
         player.draw(screen)
 
         pygame.display.flip()
